5c4a0727f718fa9dbd4f92727576bb9188fac1ef
Robert Buels <rmb32@cornell.edu>
2011-04-26 23:02:28 -0700

    move a lot of the request-building methods out of Controller::Proxy into Role::Proxy, so they can be used elsewhere

--- lib/Ambikon/IntegrationServer/Controller/Proxy.pm
+++ lib/Ambikon/IntegrationServer/Controller/Proxy.pm
@@ -7,6 +7,7 @@ use URI;
 
 BEGIN { extends 'Catalyst::Controller' }
 with 'Catalyst::Component::ApplicationAttribute';
+with 'Ambikon::IntegrationServer::Role::Proxy';
 
 __PACKAGE__->config(
     'namespace' => '',
@@ -126,96 +127,6 @@ sub make_action_code {
         );
         $cv->recv;
     }
-}
-
-
-=method build_internal_req_body
-
-figure out the body of the internal request
-
-=cut
-
-sub build_internal_req_body {
-    my ( $self, $c, $subsite, $internal_headers ) = @_;
-
-    my $type = $internal_headers->{'content-type'}
-        or return;
-
-    if( $type =~ m!^application/x-www-form-urlencoded\b!i ) {
-        my $u = URI->new;
-        $u->query_form( $c->req->body_params );
-        (my $body_string = "$u") =~ s/^\?//;
-        return $body_string;
-    }
-    elsif( $type =~ m!^multipart/form-data\b!i ) {
-        die 'multipart/form-data not yet handled';
-    }
-
-    return;
-}
-
-=method build_internal_req_url
-
-figure out the internal URL that handles a given client request
-
-=cut
-
-sub build_internal_req_url {
-    my ( $self, $c, $subsite ) = @_;
-
-    my $external_path = $subsite->external_path;
-    my $external_pq   = $c->req->uri->path_query;
-
-    my $internal_url_base  = $subsite->internal_url;
-    ( my $internal_url = $external_pq ) =~ s/^$external_path/$internal_url_base/
-        or die "cannot translate external path '$external_pq' for subsite ".$subsite->shortname;
-
-    return $internal_url;
-}
-
-=method build_internal_req_headers
-
-makes a bare hashref of headers for the internal request, using the
-user's request headers
-
-=cut
-
-sub build_internal_req_headers {
-    my ( $self, $c, $subsite ) = @_;
-
-    my %h = %{ $c->req->headers };
-    for (keys %h) {
-        delete $h{$_} if /^X-Ambikon-/i;
-    }
-
-    return \%h;
-}
-
-=method build_external_res_headers
-
-takes headers bare hashref, filters it and puts it into an
-HTTP::Headers object.
-
-=cut
-
-sub build_external_res_headers {
-    my ( $self, $c, $subsite, $headers ) = @_;
-    my $h = HTTP::Headers->new(  %$headers );
-
-    # trim off the internal host from a Location
-    if( my $l = $h->header( 'Location' ) ) {
-        $l = URI->new( $l )->canonical;
-        my $internal_host = $subsite->internal_url->clone->canonical;
-        $internal_host->path_query( '' );
-        if( $l =~ s/^$internal_host// ) {
-            $h->header('Location' => $l );
-        }
-    }
-
-    $h->header( URL => undef ); #< remove any URL header, this leaks
-                                #what the internal server is
-
-    return $h;
 }
 
 __PACKAGE__->meta->make_immutable;
