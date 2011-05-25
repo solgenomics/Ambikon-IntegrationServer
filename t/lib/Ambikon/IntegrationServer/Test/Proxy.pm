2f6bbb1216d2e6c6906327c1a55c592c823393ff
Robert Buels <rmb32@cornell.edu>
2011-05-25 12:20:56 -0700

    change the test paradigm: run the integration server as a separate process instead of in the test process

--- t/lib/Ambikon/IntegrationServer/Test/Proxy.pm
+++ t/lib/Ambikon/IntegrationServer/Test/Proxy.pm
@@ -35,11 +35,36 @@ sub test_proxy {
     my ( $port2, $port3 );
     $port2 = $servers[1]->port if $servers[1];
     $port3 = $servers[2]->port if $servers[2];
-
-    my $configuration = ref $args{conf} ? $args{conf}->( \@servers )
-                                        : eval qq|"$args{conf}"|;
+    my $configuration = ref $args{conf}
+        ? $args{conf}->( \@servers )
+        : eval qq|"$args{conf}"|;
     die $@ if $@;
-    my $mech = Ambikon::IntegrationServer::Test::WWWMechanize->new( configuration => $configuration );
+
+    # make a temp catalyst config file to feed it
+    my $temp_conf;
+    if ( $configuration ) {
+        $temp_conf = File::Temp->new( SUFFIX => '.conf' );
+        $temp_conf->print( $configuration );
+        $temp_conf->close;
+        $ENV{CATALYST_CONFIG} = $temp_conf->filename;
+    }
+
+    # start a catalyst server
+    my $ambikon_server = Test::TCP->new(
+        code => sub {
+            my ( $port ) = @_;
+            local $ENV{PLACK_SERVER} = 'Standalone';
+            my $plack = Plack::Loader->auto( port => $port, host => $host );
+            require Ambikon::IntegrationServer;
+            Ambikon::IntegrationServer->setup_engine('PSGI');
+            $plack->run( sub { Ambikon::IntegrationServer->run(@_) } );
+        },
+      );
+
+    local $ENV{CATALYST_SERVER} = 'http://localhost:'.$ambikon_server->port;
+
+    my $mech = Ambikon::IntegrationServer::Test::WWWMechanize->new( catalyst_app => 'Ambikon::IntegrationServer' );
+
     $args{client}->( $mech );
 }
 
--- t/lib/Ambikon/IntegrationServer/Test/WWWMechanize.pm
+++ t/lib/Ambikon/IntegrationServer/Test/WWWMechanize.pm
@@ -3,29 +3,7 @@ package #hide from PAUSE
 use strict;
 use warnings;
 
-use File::Temp;
-
 use parent 'Test::WWW::Mechanize::Catalyst';
 
-sub new {
-    my ( $class, %args ) = @_;
-
-    local $ENV{CATALYST_CONFIG} = $ENV{CATALYST_CONFIG};
-    local $ENV{CATALYST_SERVER} = $ENV{CATALYST_SERVER};
-
-    my $temp_conf;
-    if ( my $conf_text = delete $args{configuration} ) {
-        $temp_conf = File::Temp->new( SUFFIX => '.conf' );
-        $temp_conf->print( $conf_text );
-        $temp_conf->close;
-        $ENV{CATALYST_CONFIG} = $temp_conf->filename;
-        delete $ENV{CATALYST_SERVER};
-    }
-
-    return $class->SUPER::new(
-        catalyst_app => 'Ambikon::IntegrationServer',
-        %args,
-      );
-}
 
 1;
